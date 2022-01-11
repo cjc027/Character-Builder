11959159ee46efa7cf187a2a8537ca70253c23bc
Chris Chu <christopherjc12@gmail.com>
2022-01-11 11:50:40 -0800

    setup equipment container and delete placeholder

--- routes/equipment.js
+++ routes/equipment.js
@@ -4,5 +4,6 @@ const equipmentCtrl = require('../controllers/equipment');
 
 router.get('/characters/:id/equipment/new', equipmentCtrl.new);
 router.post('/characters/:id/equipment', equipmentCtrl.create);
+// router.delete('/equipment/:id', equipmentCtrl.delete);
 
 module.exports = router;
